3bc4da731a1b5955d434d18847a7b89d323ee179
Kobus-Smit <ksmit.aus@gmail.com>
2023-01-14 11:27:22 +1100

    Added quick start project https://github.com/Cysharp/MagicOnion#quick-start

---
+++ MyApp.Client/Program.cs
@@ -0,0 +1,8 @@
+﻿using Grpc.Net.Client;
+using MagicOnion.Client;
+using MyApp.Shared;
+
+var channel = GrpcChannel.ForAddress("https://localhost:5001");
+var client = MagicOnionClient.Create<IMyFirstService>(channel);
+var result = await client.SumAsync(123, 456);
+Console.WriteLine($"Result: {result}");
---
+++ MyApp.Server/MyFirstService.cs
@@ -0,0 +1,15 @@
+﻿using MagicOnion;
+using MagicOnion.Server;
+using MyApp.Shared;
+
+namespace MyApp.Services
+{
+    public class MyFirstService : ServiceBase<IMyFirstService>, IMyFirstService
+    {
+        public async UnaryResult<int> SumAsync(int x, int y)
+        {
+            Console.WriteLine($"Received:{x}, {y}");
+            return x + y;
+        }
+    }
+}
