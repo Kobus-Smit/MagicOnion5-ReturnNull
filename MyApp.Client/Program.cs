5157eb2223cf8ced548dcb3ca183b10b4f80cf13
Kobus-Smit <ksmit.aus@gmail.com>
2023-01-14 11:40:32 +1100

    Add startup delay and pause to client app

--- MyApp.Client/Program.cs
+++ MyApp.Client/Program.cs
@@ -4,5 +4,10 @@ using MyApp.Shared;
 
 var channel = GrpcChannel.ForAddress("https://localhost:5001");
 var client = MagicOnionClient.Create<IMyFirstService>(channel);
+
+await Task.Delay(TimeSpan.FromSeconds(2));
+
 var result = await client.SumAsync(123, 456);
 Console.WriteLine($"Result: {result}");
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
